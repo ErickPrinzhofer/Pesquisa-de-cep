bdf5b813270a99213eb8c38a7e103a01de2e8253
ErickPrinzhofer <prinzhoferestudos@gmail.com>
2024-06-05 15:46:25 -0300

    Finalizei kkkkkk

--- CEP.js
+++ CEP.js
@@ -12,28 +12,29 @@
     }
 */
 function verificar() {
-  const txtcep = document.getElementById('txtcep')
-  const resultado = document.getElementById('resultado')
-  const cep = txtcep.value
+  const txtcep = document.getElementById('txtcep')  // Pega o que o usuário digitou 
+  const resultado = document.getElementById('resultado')  //Só absorve o que vai aparecer no resultado da página
+  const cep = txtcep.value  // Dando valor ao cep, para utilizá-lo mais pra frente
 
-  const headers = { 'Content-Type': 'application/json' };
+  const headers = { 'Content-Type': 'application/json' }; // Autorização, como é uma api livre, foi só necessário dizer que é json 
   const urlCep = `https://viacep.com.br/ws/${cep}/json/`;   /// Troucar o valor do CEP por uma variavel
-  axios.get(urlCep, { headers })
-    .then(response => {
+  axios.get(urlCep, { headers }) // Método get do axios, axios foi definido no index.html
+    .then(response => { //retornando o que estava na api, mas só o que eu achei necessário
       const rua = (response.data.logradouro); //Busca o logeadouro
       const bairro = (response.data.bairro);     //bairro
       const cidade = (response.data.localidade); //cidade
       const uf = (response.data.uf);          //Tipo SP/
       const ddd = (response.data.ddd);         //DDD
 
-      resultado.innerHTML = `
+      //Abaixo é o que vai aparecer na página para o usuário
+      resultado.innerHTML = ` 
       Rua: ${rua} <br/>
       Bairro:  ${bairro} <br/>
       Cidade: ${cidade} <br/>
       UF: ${uf} <br/>
       DDD: ${ddd}`;
     })
-    .catch(error =>{
+    .catch(error => {
       resultado.innerHTML = 'Erro na busca de CEP</br>Tente novamente'
     })
 }
--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Day 1 - Testando questão de api, consegui buscar a api e retornar os dados que 
     Fiz também a parte inicial da página, pegando como exemplo o site do correios como exemplo -> https://buscacepinter.correios.com.br/app/endereco/index.php
     Lógico, não ta 100% igual, mas é a base pra fazer essa página
 Day 2 - Tentando juntar o js com o html para acessar informações do que o usuário digitou.
+    Finalizado, foi um modelo simples, mas ajudou muito no meu aprendizado
+    - Foi usado html, para criação de página. Elementos de id para modificar com o css e utilizar o JavaScript
+    - Usada uma API de CEP do correios, Digitando o CEP irá retornar: Rua, bairro, cidade, uf e DDD.
+    - A página foi inspirada no correios, como citei anteriormente, mas faltam muitos elementos para se tornar mais semelhante, mas gostei do que fiz.
+    
