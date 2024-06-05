9befa7bc0a03203ad20e32c1fcd2bad5e8192f16
ErickPrinzhofer <prinzhoferestudos@gmail.com>
2024-06-05 11:06:11 -0300

    Ainda nos testes, não consigo puxar a questão do cep

--- CEP.js
+++ CEP.js
@@ -1,23 +1,28 @@
-const axios = require('axios');
-const cep = '12943430';
-const urlCep = `https://viacep.com.br/ws/${cep}/json/`;
-const headers = {'Content-Type': 'aplication/json'}
-/**
- * cep
- * rua
- complemento
- * bairro
- * cidade
- * uf
- ibge
- gia
- * ddd
- siafi
- */
 
-axios.get(urlCep, {headers})
+/**    function pesquisar(){
+        var cep = document.getElementById('txtcep');
+        const axios = require('axios');
+        const headers = {'Content-Type': 'aplication/json'};
+        const urlCep = `https://viacep.com.br/ws/${cep}/json/`;
+        axios.get(urlCep, {headers})
+            .then(response => {
+        console.log(response.data);
+        console.log(cep)
+    });
+    }
+*/
+const axios = require('axios');
+const headers = {'Content-Type': 'aplication/json'};
+function pesquisar(){    
+    const cep = document.getElementById('txtcep');
+    const valor = cep.value;
+    const urlCep = `https://viacep.com.br/ws/${valor}/json/`;
+    axios.getAdapter(urlCep, {headers})
     .then(response => {
         console.log(response.data);
-    });
+    })
+console.log(response.data)
+    
 
+}
 
